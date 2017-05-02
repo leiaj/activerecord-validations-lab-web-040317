4db73d457b84d669767f21d833b98d0ebf012105
github username <github email address>
2017-05-02 15:40:28 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  WORDS = [
+    /Won't Believe/,
+    /Secret/,
+    /Guess/
+  ]
+
+  def clickbait
+    if WORDS.none? { |word| word.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
